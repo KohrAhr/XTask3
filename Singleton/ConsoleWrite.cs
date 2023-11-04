110b2d4fb68aa1828626b1dee85c79f30428cd08
MonsterZam <github@1click.lv>
2023-11-04 18:09:49 +0200

    Design Pattern -- Singleton and Dependency Injection

---
+++ Singleton/ConsoleWrite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class ConsoleWrite : IResult
+    {
+        public void WriteLine(string aValue) 
+        {
+            Console.WriteLine(aValue);
+        }
+
+        public void ReadKey()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/IResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Singleton
+{
+    public interface IResult
+    {
+        void WriteLine(string value);
+
+        void ReadKey();
+    }
+}
